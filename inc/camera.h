66b46f626b6d278dee1b94e08737384f9cf9c21b
Federico Di Pierro <nierro92@gmail.com>
2017-04-16 10:25:11 +0200

    Splitted clightd from clight. FIrst commit.

---
+++ inc/camera.h
@@ -0,0 +1,7 @@
+#ifndef DISABLE_FRAME_CAPTURES
+
+#include "commons.h"
+
+double capture_frames(const char *interface, int num_captures, int *err);
+
+#endif
---
+++ inc/gamma.h
@@ -0,0 +1,13 @@
+/**
+ * Taken from sct.c
+ * http://www.tedunangst.com/flak/post/sct-set-color-temperature
+ */
+
+#ifndef DISABLE_GAMMA
+
+#include "commons.h"
+
+void set_gamma(int temp, int *err);
+int get_gamma(int *err);
+
+#endif
